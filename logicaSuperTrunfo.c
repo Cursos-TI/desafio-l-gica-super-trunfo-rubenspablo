e25a908f2446e7ba04f4e9447be3b24feae10467
Rubens Pablo Suassuna Avelino <91734025+rubenspablo@users.noreply.github.com>
2025-09-23 23:18:15 +0000

    Nivel Aprendiz

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Dencidade e PibPercapta
-// Nível Mestre
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // jogo do Super Trunfo Novato
+    // jogo do Super Trunfo Novato
     // as variaveis foram declaradas juntas das duas cartas
+    int opcao;
     char estado1[40], estado2[40];
     char codigo1[10], codigo2[10];
     char cidade1[50], cidade2[50];
@@ -20,9 +19,19 @@ int main() {
     float pibPercapta1, pibPercapta2;
     double SuperPoder1, SuperPoder2; // Variáveis para o Super Poder
     
-    
-    // Apresentação do jogo
-    printf("**** SUPER TRUNFO ****\n");
+
+    printf("#### Seja bem vindo ao Super Trunfo ####\n");
+    printf("Aqui esta o menu de opções:\n");
+    printf("1 - Jogar\n");
+    printf("2 - Instruções\n");
+    printf("3 - Sair\n");
+    printf("Digite a opção desejada: ");
+    scanf("%d", &opcao);
+    switch (opcao)
+    {
+    case 1:
+         // Apresentação do jogo
+    printf("\n**** SUPER TRUNFO ****\n");
     printf("Olá jogador, seja bem vindo ao Super Trunfo! Um Mundo de disputas e guerras.\n");
     printf("Onde cada jogador enfrenta desafios jamais vistos, e batalhas épicas são vistas a todo o momento.\n");
     printf("Para esse desafio teremos que montar suas cartas desafiadoras.\n");
@@ -161,52 +170,108 @@ int main() {
     printf("PIB Per Capta: %.2f bilhões de Reais por habitante\n\n", pibPercapta2);
     printf("Super Poder: %.2f\n\n", SuperPoder2);
 
-    //Comparação das Cartas
-    printf("Vamos comparar as cartas\n");
-    if (populacao1 > populacao2) {
-        printf("A carta 1 tem a maior população\n");
-    } else if (populacao1 < populacao2) {
-        printf("A carta 2 tem a maior população\n");
+    // escolha do atributo da carta para comparação
+    printf("Escolha o atributo que você quer comparar entre as cartas:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+    printf("6 - PIB Per Capta\n");
+    printf("7 - Super Poder\n");
+    printf("Digite o número do atributo desejado para competição entre as cartas: ");
+    int atributo;
+    scanf("%d", &atributo);
+    switch (atributo) {
+    case 1:
+         printf("Você escolheu comparar a População.\n");
+         if (populacao1 > populacao2) {
+            printf("A carta 1 tem a maior população: %d habitantes.\n", populacao1);
+        } else if (populacao1 < populacao2) {
+            printf("A carta 2 tem a maior população: %d habitantes.\n", populacao2);
+        } else {
+            printf("As duas cartas têm a mesma população: %d habitantes.\n", populacao1);  
+        }
+        break;
+    case 2:
+        printf("Você escolheu comparar a Área.\n");
+        if (area1 > area2) {
+            printf("A carta 1 tem a maior área: %.2f Km².\n", area1);
+        } else if (area1 < area2) {
+            printf("A carta 2 tem a maior área: %.2f Km².\n", area2);
+        } else {
+            printf("As duas cartas têm a mesma área: %.2f Km².\n", area1);  
+        }
+        break;
+    case 3:
+        printf("Você escolheu comparar o PIB.\n");
+        if (pib1 > pib2) {
+            printf("A carta 1 tem o maior PIB: %.2f bilhões de Reais.\n", pib1);
+        } else if (pib1 < pib2) {
+            printf("A carta 2 tem o maior PIB: %.2f bilhões de Reais.\n", pib2);
+        } else {
+            printf("As duas cartas têm o mesmo PIB: %.2f bilhões de Reais.\n", pib1);  
+        }
+        break;
+    case 4:
+        printf("Você escolheu comparar os Pontos Turísticos.\n");
+        if (pontos1 > pontos2) {
+            printf("A carta 1 tem mais pontos turísticos: %d pontos.\n", pontos1);
+        } else if (pontos1 < pontos2) {
+            printf("A carta 2 tem mais pontos turísticos: %d pontos.\n", pontos2);
+        } else {
+            printf("As duas cartas têm o mesmo número de pontos turísticos: %d pontos.\n", pontos1);  
+        }
+        break;
+    case 5:
+        printf("Você escolheu comparar a Densidade Demográfica.\n");
+        if (densidade1 > densidade2) {
+            printf("A carta 2 vence com a menor densidade: %.2f habitantes por Km².\n", densidade2);
+        } else if (densidade1 < densidade2) {
+            printf("A carta 1 vence com a menor densidade: %.2f habitantes por Km².\n", densidade1);
+        } else {
+            printf("As duas cartas têm a mesma densidade demográfica: %.2f habitantes por Km².\n", densidade1);  
+        }
+        break;
+    case 6:
+        printf("Você escolheu comparar o PIB Per Capta.\n");
+        if (pibPercapta1 > pibPercapta2) {
+            printf("A carta 1 tem o maior PIB Per Capta: %.2f bilhões de Reais por habitante.\n", pibPercapta1);
+        } else if (pibPercapta1 < pibPercapta2) {
+            printf("A carta 2 tem o maior PIB Per Capta: %.2f bilhões de Reais por habitante.\n", pibPercapta2);
+        } else {
+            printf("As duas cartas têm o mesmo PIB Per Capta: %.2f bilhões de Reais por habitante.\n", pibPercapta1);  
+        }
+        break;
+    case 7:
+        printf("Você escolheu comparar o Super Poder.\n");
+        if (SuperPoder1 > SuperPoder2) {
+            printf("A carta 1 é a vencedora com o maior Super Poder: %.2f\n", SuperPoder1);
+        } else if (SuperPoder1 < SuperPoder2) {
+            printf("A carta 2 é a vencedora com o maior Super Poder: %.2f\n", SuperPoder2);
+        } else {
+            printf("As duas cartas têm o mesmo Super Poder: %.2f\n", SuperPoder1);  
+        }
+        break;
     }
+        break;  
+    case 2:
+        printf("Instruções do Jogo:\n");
+        printf("1. Você terá direito a duas cartas, cada uma representando uma cidade com diversos atributos.\n");
+        printf("2. Cada carta terá os seguintes atributos: Estado, Código, Cidade, População, Área, PIB, Pontos Turísticos, Densidade Demográfica, PIB Per Capta e Super Poder.\n");
+        printf("3. Após criar suas cartas, você poderá escolher um atributo para comparar entre as duas cartas.\n");
+        printf("4. O objetivo é ter a carta com o maior valor no atributo escolhido.\n");
+        printf("5. Seja estratégico na escolha do atributo para maximizar suas chances de vitória!\n");
+        printf("6. Divirta-se e boa sorte!\n");
+        break;
+    case 3:
+        printf("Saindo do jogo. Até a próxima!\n");
+        break;
+    default:
+        printf("Opção inválida. Por favor, escolha uma opção válida.\n");
+        break;
 
-    if (area1 > area2) {
-        printf("A carta 1 tem a maior área\n");
-    } else if (area1 < area2) {
-        printf("A carta 2 tem a maior área\n");
-    }
+}
 
-    if (pib1 > pib2) {
-        printf("A carta 1 tem o maior PIB\n");
-    } else if (pib1 < pib2) {
-        printf("A carta 2 tem o maior PIB\n");
-    }
-
-    if (pontos1 > pontos2) {
-        printf("A carta 1 tem mais pontos turísticos\n");
-    } else if (pontos1 < pontos2) {
-        printf("A carta 2 tem mais pontos turísticos\n");
-    }
-
-    if (densidade1 > densidade2) {
-        printf("A carta 2 vence\n");
-    } else if (densidade1 < densidade2) {
-        printf("A carta 1 vence\n");
-    }
-
-    if (pibPercapta1 > pibPercapta2) {
-        printf("A carta 1 tem o maior PIB Per Capta\n");
-    } else if (pibPercapta1 < pibPercapta2) {
-        printf("A carta 2 tem o maior PIB Per Capta\n");
-    }
-    
-    if  (SuperPoder1 > SuperPoder2) {
-        printf("A carta 1 é a vencedora com o maior Super Poder\n");
-    } else if (SuperPoder1 < SuperPoder2) {
-        printf("A carta 2 é a vencedora com o maior Super Poder\n");
-    } else {
-        printf("As duas cartas tem o mesmo Super Poder\n");
-    }
-    
-    
     return 0;
 }
